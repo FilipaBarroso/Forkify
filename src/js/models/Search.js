ce76ab8f4601ef1635f87341c41e05d8be6da8b9
FilipaBarroso <up201307852@fe.up.pt>
2019-05-26 20:18:42 +0100

    first commit

---
+++ src/js/models/Search.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = 'afa12cf587534d133923e24f08fd13a4';
+        try {
+            const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            //console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
