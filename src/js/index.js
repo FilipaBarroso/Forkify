92532874f0febd8106deddfb2fdc93968ff1f2c0
FilipaBarroso <up201307852@fe.up.pt>
2019-05-27 17:58:28 +0100

    search for recipes

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import {elements, renderLoader, clearLoader} from './views/base';
 
 
 /** Global state of the app
@@ -7,27 +9,41 @@ import Search from './models/Search';
  * - shopping list object
  * - liked recipes
  */
-const state = {}
+const state = {};
 
 const controlSearch = async () => {
     // 1. get query from view
-    const query = 'pizza'; //TODO
+    const query = searchView.getInput();
 
     if (query){
         // 2. new search object and add to state
         state.search = new Search(query);
 
         // 3. prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
 
         // 4. search for recipes
         await state.search.getResults();
 
         // 5. render results on UI
-        console.log(state.search.result);
+        clearLoader();
+        searchView.renderResults(state.search.result);
     }
-}
+};
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+        console.log(goToPage);
+    }
 })
